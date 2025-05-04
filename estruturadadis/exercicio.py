6555d2ec5401c743bb701ec3a5bb9b595eed2f7c
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-04 01:29:44 -0300

    um pouco de exericios haha

---
+++ estruturadadis/exercicio.py
@@ -0,0 +1,15 @@
+#pagina 72 exercicio 1
+"""
+x = int(input(" digite aqui o 1º valor : R"))
+z = int(input("digite aqui o 2º valor : R"))
+soma = x + z
+
+print(soma)
+"""
+
+## converter metros em milimetros
+"""
+x = float(input("Digite aqui o valor em metros que você deseja converter em milímetros: "))
+convertido = x * 1000
+print(convertido)
+"""
