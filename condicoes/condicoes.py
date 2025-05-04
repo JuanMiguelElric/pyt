5b741539f6d7be4d904693457904fcfc6119c056
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-04 01:41:30 -0300

    nada como brincar um pouco com nosso antigo amigo if

---
+++ condicoes/condicoes.py
@@ -0,0 +1,30 @@
+"""
+agora chegamos em uma parte muito importante do python onde devemos mecher como condições em python sim aquele famoso 
+e se 
+-___-
+if <condição>:
+    bloco verdadeiro
+
+mano olha que estrutura que é linda ou assustadora para você ?
+"""
+
+"""
+a = int(input("Primeiro valor:"))
+b = int(input("Segundo valor:"))
+if a > b:
+    print("primeiro valor é maior amigo ")
+if b > a:
+    print("segundo valor é maior amigo ")
+"""
+
+## simplês né ? mas calma que ainda não acabou por aqui ainda temos muitas coisas para se fazer e vamos colocar aqui para dar sequência ainda 
+"""
+idade = int(input("digite aqui o valor do seu notebook meu irmão, pode ser pc também"))
+
+if idade > 3:
+    print("pc um pouco antigo ein ")
+if idade < 3:
+    print("pc bem novinho ein")
+
+"""
+### olha que lindo ein como é facil!! mas vai dizer que não dá saudade das nossas chaves {} e parenteses () hahaha[
