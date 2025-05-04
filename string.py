1d46d9603204ed22edbc4e2d466cc957d08c2a51
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-04 01:13:26 -0300

    estudo de e string dentro de python foi aprendido a concatenar, coo mexer com os tipos de escritas dentro do mundo python principalmento o mode de se imprimilas dentro do nosso programa

---
+++ string.py
@@ -0,0 +1,33 @@
+#s = 'ABC'
+#print(s + 'c')
+## isso daqui é um meio de concatenação em python  que só pode ser utilizada em string 
+
+##x = 20
+##print("juan miguel tem %d anos" % x)
+## essa forma é utilizada para colocar por exemplo um numero uma data ou alguma mensagem isso daqui é bem top para se utilziar
+# pega sequencia %d == numeros inteiros %s == strings %f numeros decimais 
+
+
+
+## outra maneira importante
+"""
+nome = 'juan'
+idade = 23
+ano = 2025
+tipo ='masculino'
+
+print("Salve! Me chamo {} tenho a idade de: {}, atualmente estou no ano de {}, isso mesmo estou no seculo 21 na terceira decada deste mesmo século e sou do genero {}".format(nome,idade,ano,tipo))
+"""
+"""
+s = 'ABCDEFGHJ'
+print(s[0:2])
+
+## resultado AB
+
+print(s[1:2])
+
+## resultado B
+"""
+
+
+
