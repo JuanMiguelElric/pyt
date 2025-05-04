2e4cb94985906893660ec06378e1651ee2ee5c52
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-04 01:22:26 -0300

    entrada de dados em python pelo terminal ein lembrando daquele velho tempode c kkkk

---
+++ estruturadadis/parte1.py
@@ -0,0 +1,18 @@
+##destinado a apenas estrutura de dados
+### entrada de dados de python é utilizando o input() isso mesmo 
+"""x = input("digite aqui um numero:")
+print(x)"""
+
+"""
+nome = input("Digite aqui o seu nome meu nobre")
+print(f"Você digitou {nome}")
+"""
+
+## gostou né agora se liga oque foi escrito recebe apenas como string mas e se eu precisar tratar em ponto flutuante ou em inteiro 
+"""
+ano = int(input("tempo de serviço: "))
+valor_por_ano = float(input("Quantos você receb por ano ? "))
+bonus = ano * valor_por_ano
+
+print(f"Bônus de R$ {bonus:5.2f}")
+"""
