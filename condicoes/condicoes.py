cfe5ccd62f7c4f66856954a7d514f2f6118aca50
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-04 17:16:23 -0300

    complemento das condiçoes adicionando else e elif

--- condicoes/condicoes.py
+++ condicoes/condicoes.py
@@ -28,3 +28,21 @@ if idade < 3:
 
 """
 ### olha que lindo ein como é facil!! mas vai dizer que não dá saudade das nossas chaves {} e parenteses () hahaha[
+
+
+### agora chegou a hora de voltar o nosso else ? creio que ainda é bem cedo para isso kkk  deixa ele descansar um pouco né ainda depois trazemos o maldito
+
+
+
+
+
+"""
+x = input("Digite aqui qual é o nome do criador")
+if x == "juan":
+    print("olá juan tudo bem com você?")
+else:
+    print(f"{x} não foi quem criou esse programa")
+
+"""
+
+## em python o else if que conheço em php e outras linguagens é utlizado com elif, sim que nome mais top daria até para colocar em um ganso de tão lindo que é 
