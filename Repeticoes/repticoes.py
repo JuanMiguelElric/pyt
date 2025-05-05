6801610bfeeb8adc46b550af0c8741a7c9436265
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-05 16:06:37 -0300

    mai una

--- Repeticoes/repticoes.py
+++ Repeticoes/repticoes.py
@@ -1,9 +1,23 @@
 ### estamos dando aquela linda entrada em repetições 
-
+"""
 x = 1 
 while x <= 100:
     print(x)
     x = x + 1
 
-""" olha que magnifico como é feito o while em python da até gosto de ver né ?
+ olha que magnifico como é feito o while em python da até gosto de ver né ?
 """
+"""
+fim = int(input("digite aqui um numero:"))
+x=1
+while x <= fim:
+    print(x)
+    x = x + 1
+"""
+tabuada = 1
+while tabuada <= 10:
+    numero = 1
+    while numero <= 10:
+        print(f"{tabuada} x {numero} = {tabuada * numero}")
+        numero += 1
+    tabuada += 1
