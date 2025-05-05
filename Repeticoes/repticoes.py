0041d1ec5ac62e95cfaa3a6b4e6ec8a304d98fc1
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-05 02:06:16 -0300

    repeticoes com while

---
+++ Repeticoes/repticoes.py
@@ -0,0 +1,9 @@
+### estamos dando aquela linda entrada em repetições 
+
+x = 1 
+while x <= 100:
+    print(x)
+    x = x + 1
+
+""" olha que magnifico como é feito o while em python da até gosto de ver né ?
+"""
