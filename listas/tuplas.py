b67a1e9334a70e9a63e03ae578becacb8ec503a3
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-08 17:11:26 -0300

    tuplas

---
+++ listas/tuplas.py
@@ -0,0 +1,12 @@
+"""
+tupla = ("a","b","c")
+print(tupla)
+
+esse é um lindo jeito de se apresentar uma tupla mas pode ser apresentada também sem parenteses
+
+"""
+*a,b =[1,2,3,4, 5]
+print(a)
+print(b)
+##*a,b dizemos: coloque o ultimo valor em b e os restante em a  em a,*b significa coloque o primeiro valor em a e o restante em b
+
