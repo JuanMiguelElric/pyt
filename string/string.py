5289482c153e0cfb8549b66fc9a865ad51d1cbe8
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-09 00:51:36 -0300

    primeira parte de string

---
+++ string/string.py
@@ -0,0 +1,19 @@
+## contagem
+"""
+t = "um tigre, dois tigres, três tigres"
+t.count("i")
+"""
+##pesquisar string
+s = "Alõ mundo"
+print(s.find("mun"))
+
+## caso o objetivo seja procurar da direita para a esquerda tilize o metodo rfind
+
+v = "tigre"
+print(v.ljust(20))
+
+## l just é para completar com espaçõs a esquerda
+
+##.strip() é utilizado para remover espaçoes em brancos do inicio ao fim
+
+## metodo isprintable() = é utilizado para verificar se algo foi impresso na tela 
