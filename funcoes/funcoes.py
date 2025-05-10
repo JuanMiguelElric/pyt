364f64e19660db7a1fe3666bf93c4270ab2ca155
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-09 22:15:03 -0300

    funçoes em python

---
+++ funcoes/funcoes.py
@@ -0,0 +1,22 @@
+## funçoes em python se utiliza o def parceirinho
+
+def soma(a,b):
+    return a + b
+print(soma(2,2))
+
+
+## função que retorna se o número é para meu parceiro 
+
+def epar(x):
+    return x % 2 ==0
+print(epar(2))
+print(epar(3))
+
+def maximo(x,y):
+    if x > y:
+        return x
+    else:
+        return y
+print("qual é maior")   
+print(maximo(2,6))
+print(maximo(5,2))
