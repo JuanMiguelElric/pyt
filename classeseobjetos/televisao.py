ad5e156e906c99f62cb1ab98fc3efd48f26255de
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-21 01:35:03 -0300

    inicio de classe e objetos

---
+++ classeseobjetos/televisao.py
@@ -0,0 +1,10 @@
+class Televisao:
+    def __init__(self):
+        self.ligada = False
+        self.canal = 2
+
+
+t = Televisao()
+## está chamando a classe televisao 
+
+t.ligada ## irá retornar o valor da  função
