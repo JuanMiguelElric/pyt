9b373f9fc907bcaaabbbe599f33529c5b8dc171e
JuanMiguelElric <oliveirajuan243@gmail.com>
2025-05-21 08:53:50 -0300

    nova classe televisao

--- classeseobjetos/televisao.py
+++ classeseobjetos/televisao.py
@@ -1,12 +1,20 @@
 class Televisao:
-    def __init__(self):
+    def __init__(self, min, max):
         self.ligada = False
-    
-    def canal(self):
-        return 3
+        self.canal = 2
+        self.cmin = min
+        self.cmax = max
+    def muda_canal_para_baixo(self):
+        if self.canal - 1 >= self.cmin:
+            self.canal -= 1
+    def muda_canal_para_cima(self):
+        if self.canal + 1 <= self.cmax:
+            self.canal += 1
 
 
 t = Televisao()
 
 print(t.ligada)  # False
-print(t.canal()) # 3
+t.muda_canal_para_baixo()
+print(t.canal)
+print(t.muda_canal_para_cima())
